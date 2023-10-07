bb15f0193709c20f82ea36f36873441902d30dd6
Andrii Poltoratskyi <anp2937@g.harvard.edu>
2023-10-07 03:29:22 +0000

    BlackJack game: implemented UI and button actions.

---
+++ p1/index-view.php
@@ -0,0 +1,40 @@
+</html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Black Jack GAME</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+    <h1>Black Jack GAME</h1>
+    <div class="row">
+        <div class="column">
+            <div class="player">
+                <?php echo $_SESSION["playerCards"] ?>
+            </div>
+        </div>
+        <div class="column">
+            <div class="dealer">
+            <?php echo $_SESSION["dealerCards"] ?>
+            </div>
+        </div>
+    </div>
+    </main>
+
+<br><br>
+    <footer>
+        <div class="footer-content">
+            <form method="post" id="buttons">
+                <button type="submit" name="start">Start the Game</button>
+                <button type="submit" name="hit">Hit</button>
+                <button type="submit" name="stay">Stay</button>
+                <button type="submit" name="reset">Reset</button>
+            </form>
+        </div>
+        
+    </footer> 
+    
+</body>
+
+</html>
---
+++ p1/index.php
@@ -0,0 +1,103 @@
+<?php
+
+// Start the session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+// function to render players current conbination of cusrd on the deck
+function playerDeck($playerCards, &$cards) {
+
+    if(!$playerCards){
+        $playerCards .= getOneCard($cards);
+    }
+    $playerCards .= getOneCard($cards);
+
+    if(!$_SESSION['playerCards']){
+        $_SESSION['playerCards'] = $playerCards;
+    } else {
+        $_SESSION['playerCards'] .= $playerCards;
+    }
+
+}
+
+// function to render dealer current conbination of cusrd on the deck
+function dealerDeck($dealerCards, &$cards) {
+
+    //if this is begining of the game - add the first one 
+    if (!$dealerCards){
+        $dealerCards = '<img src="img/card_back.png" alt="">';
+    }
+
+    $dealerCards .= getOneCard($cards);
+
+    $_SESSION["dealerCards"] .= $dealerCards;
+}
+
+
+//get top card from the cards stack
+function getOneCard(&$cards) {
+
+    $randCardName = array_pop($cards);
+    $card = "<img src='img/cards/{$randCardName}' alt=''>";
+    return $card;
+}
+
+
+//get list of cards from img/cards directory
+// https://code.google.com/archive/p/vector-playing-cards/downloads
+function getAllCards() {
+    
+    $cards = scandir("img/cards");
+    // remove two first elements "." and ".." of folder hierarchy
+    array_splice($cards, 0, 2);
+    shuffle($cards);
+
+    return $cards;
+}
+
+
+function hit (&$cards, $playerDeck) {
+    $newCardName = getOneCard($cards);
+    $_SESSION['playerCards'] .= $newCardName;
+}
+
+// playerDeck($playerCards, $_SESSION["cards"]);
+// dealerDeck($dealerCards, $_SESSION["cards"]);
+// hit($_SESSION["cards"], $_SESSION["playerCards"]);
+
+if(array_key_exists('start', $_POST)) {
+
+    $playerCards = '';
+    $dealerCards = '';
+
+    $_SESSION["cards"] = getAllCards();
+    $_SESSION["playerCards"] = '';
+    $_SESSION["dealerCards"] = '';
+
+    // init player deck
+    playerDeck($playerCards, $_SESSION["cards"]);
+    //init dealer deck
+    dealerDeck($dealerCards, $_SESSION["cards"]);
+}
+
+if(array_key_exists('hit', $_POST)) {
+    hit($_SESSION["cards"], $_SESSION["playerCards"]);
+}
+
+if(array_key_exists('stay', $_POST)) {
+    $newCardName = getOneCard($_SESSION["cards"]);
+    $_SESSION['dealerCards'] .= $newCardName;
+}
+
+if(array_key_exists('reset', $_POST)) {
+    if (session_status() === PHP_SESSION_ACTIVE) {
+        session_destroy();
+        $_SESSION["playerCards"] = '';
+        $_SESSION["dealerCards"] = '';
+     }
+}
+
+
+include 'index-view.php';
